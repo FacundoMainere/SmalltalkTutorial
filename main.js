92281df5cdc6c12c03b50fb4a7c102860d2a9a7d
Francisco Demartino <demartino.francisco@gmail.com>
2012-07-28 05:02:06 -0300

    simplificado main.js

--- main.js
+++ main.js
@@ -8,22 +8,6 @@ var fbId= "138391069632276";
 var fbSecret= "662321b535c93082a88378ff4c468e60";
 var fbCallbackAddress= "http://smalltalktutorial.herokuapp.com/auth/facebook_callback"
 
-
-var specialPaths = {
-    loginFailure: "/loginFailed"
-  , accessDenied: "/accessDenied"
-  , secret: "/secret"
-  , login: "/login"
-  , logout: "/logout"
-  , justLoggedIn: "/justLoggedIn"
-};
-
-var authenticatedRoutes= {
-      "/login": {authenticate: true }
-    , "/justLoggedIn": {authenticate: true }
-    , "/secret": {authenticate: false }
-};
-
 function redirect(req, res, location) {
   res.writeHead(303, { 'Location': location });
   res.end('');
@@ -32,96 +16,53 @@ function redirect(req, res, location) {
 function renderFile(res, filename, options) {
 	res.end(ejs.render(fs.readFileSync(__dirname + '/templates/' + filename, 'utf8'), options));
 }
-// To demonstrate a middleware-type approach, provide an example one
-var example_auth_middleware= function() {
-  return function(req, res, next) {
-    var urlp= url.parse(req.url, true)
-    var path= urlp.pathname;
-    if( path ==  specialPaths.logout ) {
-      req.logout(function() {
-        next();
-      });
-    }
-    else {
-      if( authenticatedRoutes[path] ) {
-        if( authenticatedRoutes[path].authenticate && authenticatedRoutes[path].authenticate === true ) {
-          req.authenticate( function(error, authenticated){
-            if( authenticated === true ) next();
-            else if( authenticated === false ) redirect( req, res, specialPaths.loginFailure );
-          });
-        } else {
-          if( req.isAuthenticated() ) next();
-          else redirect( req, res, specialPaths.accessDenied );
-        }
-      } else next();
-    }
-  }
+
+var smalltalk_tutorial_middleware = function() {
+	return function (req, res, next) {
+		var path = url.parse(req.url, true).pathname;
+		if (path == "/logout") {
+			req.logout(function() {
+				next();
+			});
+		} else if (path == "/login") {
+			req.authenticate(function (error, authenticated){
+				if( authenticated === true ) next();
+				else if( authenticated === false ) redirect( req, res, "/fail" );
+			});	
+		} else next();
+	}
 };
 
 var users= {};
 
-// Utilise the 'events' to do something on first load (test whether the user exists etc. etc. ) 
 function firstLoginHandler( authContext, executionResult, callback ) {
-
-  // The originally request URL will be stored in : executionResult.originalUrl 
-  // this could be used for redirection in 'real' cases.
-  if( users[executionResult.user.id] ) {
-    //console.log('Known USER: ' + executionResult.user.id);
-    redirect( authContext.request, authContext.response, "/");
-  } else {
-    // So here one would probably 'register' the user in the local system.
-    //console.log('Brand new USER: ' + executionResult.user.id);
-    users[executionResult.user.id]= true;
-    redirect( authContext.request, authContext.response, specialPaths.justLoggedIn );
-  }
+	// executionResult.originalUrl 
+	if( ! users[executionResult.user.id] ) { // if not registered
+		users[executionResult.user.id]= true; //register
+	}
+	redirect( authContext.request, authContext.response, "/");
 }
 
-// Define our pages
-// a 'Home page' 
-// a 'Failed login page'
-// a 'Logout page'
-// a 'Secrets page' (requires authentication (but won't attempt to authenticate) )
 function routes(app) {
-  app.get(specialPaths.secret, function(req, res, params) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-	renderFile(res, 'pedo.html', {
-		fbid: req.getAuthDetails().user.id
-	});
-  });
-  app.get(specialPaths.accessDenied, function(req, res, params) {
-    res.writeHead(403, {'Content-Type': 'text/html'})
-    res.end("Acces denied.")
-  });
-  app.get(specialPaths.login, function(req, res, params) {
-    redirect( req, res, "/" );
-  }); 
-  app.get(specialPaths.loginFailure, function(req, res, params) {
-    res.end("<html><body>Authentication error, please <a href='/login'>try again</a>.</body></html>")
-  }); 
-  app.get(specialPaths.justLoggedIn, function(req, res, params) {
-    //console.log("Es realmente puto el que lee? Muchas gracias. AH SE REGISTRO UN FLACO")
-    redirect( req, res, "/" );
-  }); 
 
-  app.get(/.*/, function(req, res, params) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    if( req.isAuthenticated() ) {
-		det = req.getAuthDetails();
-		renderFile(res, 'index.html', {
-			lesson_number: 1,
-			user: det.user
-		});
-    }
-    else {
-      res.end("<html><body>Please <a href='/login'>Login</a> <br /> <a href='/secret'>Shhh! Secrets</a></body></html>")
-    }
-  })
+	app.get("/fail", function(req, res, params) {
+		res.end("<html><body>Authentication error, please <a href='/login'>try again</a>.</body></html>")
+	}); 
+
+	app.get(/.*/, function(req, res, params) {
+		res.writeHead(200, {'Content-Type': 'text/html'})
+		if( req.isAuthenticated() ) {
+			det = req.getAuthDetails();
+			renderFile(res, 'index.html', {
+				lesson_number: 1,
+				user: det.user
+			});
+		} else {
+			renderFile(res, 'home-noauth.html');
+		}
+	})
 }
-/*
-connect.createServer(
-		connect.static(__dirname + '/public')
-).listen(process.env.PORT);
-*/
+
 connect.createServer(
 	  connect.static(__dirname + '/public')
 	, connect.cookieParser()
@@ -129,12 +70,6 @@ connect.createServer(
 	, auth( {  strategies: auth.Facebook({appId : fbId, appSecret: fbSecret, scope: "", callback: fbCallbackAddress})
 			, trace: true
 			, firstLoginHandler: firstLoginHandler } )
-	, example_auth_middleware()
+	, smalltalk_tutorial_middleware()
 	, connect.router(routes)
 ).listen(process.env.PORT);
-
-/*
-connect.createServer(
-    connect.static(__dirname)
-).listen(process.env.PORT);
-*/

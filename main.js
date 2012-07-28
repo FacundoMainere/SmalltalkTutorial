b2793cfb1239d0b08609290d96c6ba1e785b63f1
Francisco Demartino <demartino.francisco@gmail.com>
2012-07-28 00:46:03 -0300

    MANKED

--- main.js
+++ main.js
@@ -114,10 +114,7 @@ function routes(app) {
     }
   })
 }
-connect.createServer(
-		connect.static(__dirname + '/public')
-).listen(process.env.PORT);
-	
+
 connect.createServer(
 	  connect.static(__dirname + '/public')
 	, connect.cookieParser()

7b9787c1985b0577943a4cbb33c40b9a1f3bfec6
Francisco Demartino <demartino.francisco@gmail.com>
2012-07-28 00:11:53 -0300

    Pagina secreta en subdir templates

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var  connect = require('connect')
    , auth= require('./lib')
    , url= require('url')
-   , ejs= require('ejs');
+   , ejs= require('ejs')
+   , fs= require('fs');
 
 var fbId= "138391069632276";
 var fbSecret= "662321b535c93082a88378ff4c468e60";
@@ -28,6 +29,9 @@ function redirect(req, res, location) {
   res.end('');
 }
 
+function renderFile(res, filename, options) {
+	res.end(ejs.render(fs.readFileSync(__dirname + '/templates/' + filename, 'utf8'), options));
+}
 // To demonstrate a middleware-type approach, provide an example one
 var example_auth_middleware= function() {
   return function(req, res, next) {
@@ -80,26 +84,22 @@ function firstLoginHandler( authContext, executionResult, callback ) {
 function routes(app) {
   app.get(specialPaths.secret, function(req, res, params) {
     res.writeHead(200, {'Content-Type': 'text/html'})
-	res.end(ejs.render('<html><body><h2>Me tire <%= pedo%> pedo. <%=fbid%></h2></body></html>', {pedo: 1, fbid: req.getAuthDetails().user.id}));
-	/*
-	var wachi = "<div id='fb-root'></div><script src='http://connect.facebook.net/en_US/all.js'></script><p><a onclick='postToFeed(); return false;'>Post to Feed</a></p><p id='msg'></p><script> FB.init({appId: 138391069632276, status: true, cookie: true});function postToFeed() { var obj = {method: 'feed', link: 'http://smalltalktutorial.herokuapp.com/secret', picture: 'http://smalltalktutorial.herokuapp.com/favicon.ico',name: 'WA',caption: 'CHI',description: 'TU RROS'};function callback(response) {document.getElementById('msg').innerHTML = 'Post ID: ' + response['post_id'];}FB.ui(obj, callback);      } </script>"	;
-
-    res.end('<html><body>HERE BE UNICORNS... Special Secret unicorns.<br><iframe width="560" height="315" src="http://www.youtube.com/embed/IR__WMtmN90" frameborder="0" allowfullscreen></iframe>' + wachi + '</body></html>')
-	*/
-	
+	renderFile(res, 'pedo.js', {
+		fbid: req.getAuthDetails().user.id
+	});
   });
   app.get(specialPaths.accessDenied, function(req, res, params) {
     res.writeHead(403, {'Content-Type': 'text/html'})
-    res.end("Oh-No-You-Didn't! You are not authenticated, be-gone.")
+    res.end("Acces denied.")
   });
   app.get(specialPaths.login, function(req, res, params) {
     redirect( req, res, "/" );
   }); 
   app.get(specialPaths.loginFailure, function(req, res, params) {
-    res.end("<html><body>Facebook no like you :( Please <a href='/login'>Try again!</a> </body></html>")
+    res.end("<html><body>Authentication error, please <a href='/login'>try again</a>.</body></html>")
   }); 
   app.get(specialPaths.justLoggedIn, function(req, res, params) {
-    console.log("Es realmente puto el que lee? Muchas gracias. AH SE REGISTRO UN FLACO")
+    //console.log("Es realmente puto el que lee? Muchas gracias. AH SE REGISTRO UN FLACO")
     redirect( req, res, "/" );
   }); 
 

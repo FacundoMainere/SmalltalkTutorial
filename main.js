f47aabf3cd0140811ee3f2f6818138cc690a262a
Francisco Demartino <demartino.francisco@gmail.com>
2012-08-18 05:29:17 -0300

    sir, u fail at json

--- main.js
+++ main.js
@@ -80,7 +80,9 @@ function firstLoginHandler( authContext, executionResult, callback ) {
 	console.log(sql);
 	sqlconn.query(sql,
 	function(err, a, b) {
-		console.log(JSON.stringify({ a, b});
+	
+		console.log('a: '+JSON.stringify(a);
+		console.log('b: '+JSON.stringify(b);
 	});
 	
 	if(  ret == 0 ) { 
